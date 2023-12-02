6bca7bf15b92a42508fd1fe218efaa224953d897
Frank Developr <frank.developr@gmail.com>
2023-12-02 20:09:55 +0100

    feature: working game!

---
+++ src/Aoc2023Day2/GameCalculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aoc2023Day2;
+
+public class GameCalculator
+{
+    private readonly IEnumerable<Game> _games;
+
+    public GameCalculator(IEnumerable<Game> games)
+	{
+		_games = games;
+	}
+
+    public int PossibleGamesSumId(CubeSet set)
+    {
+        var possible = _games.Where(g => g.AcceptedCubeSet.FitsIn(set));
+
+        return possible.Sum(t => t.Id);
+    }
+}
---
+++ test/Aoc2023Day2Test/GameCalculatorTests.cs
@@ -0,0 +1,23 @@
+﻿using Aoc2023Day2;
+using FluentAssertions;
+
+namespace Aoc2023Day2Test;
+
+public class GameCalculatorTests
+{
+    [Theory]
+    [InlineData("test2-1.txt", 8)]
+    [InlineData("problem2.txt", 2331)]
+    public void Given_TestInput_Then_Returns8(string file, int expectedIdSum)
+    {
+        var lines = File.ReadAllLines($"data/{file}");
+        var parser = new GameParser();
+        var games = parser.Parse(lines);
+
+        var calculator = new GameCalculator(games);
+
+        var result = calculator.PossibleGamesSumId(new CubeSet(12, 13, 14));
+
+        result.Should().Be(expectedIdSum);
+    }
+}
