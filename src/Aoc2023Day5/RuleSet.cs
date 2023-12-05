5c35f7b972a1462daeb933bd5918f91c668861c5
Frank Developr <frank.developr@gmail.com>
2023-12-05 20:34:47 +0100

    feature: working copy of ruleset

---
+++ src/Aoc2023Day5/RuleSet.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Aoc2023Day5;
+
+public class RuleSet
+{
+    private readonly IEnumerable<Rule> _rules;
+
+    public RuleSet(IEnumerable<Rule> rules)
+    {
+        _rules = rules;
+    }
+
+    public int Apply(int input)
+    {
+        foreach (var rule in _rules)
+        {
+            if (rule.TryApply(input, out var output))
+            {
+                return output;
+            }
+        }
+
+        return input;
+    }
+}
