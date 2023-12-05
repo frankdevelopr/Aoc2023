f920d02e4a04573d95d7998c3e4504d217d40fb2
Frank Developr <frank.developr@gmail.com>
2023-12-05 20:19:37 +0100

    feature: Rule working

---
+++ src/Aoc2023Day5/Rule.cs
@@ -0,0 +1,28 @@
+﻿namespace Aoc2023Day5;
+
+public class Rule
+{
+    private readonly int _destinationRange;
+    private readonly int _sourceRange;
+    private readonly int _sourceRangeEnd;
+
+    public Rule(int destinationRange, int sourceRange, int rangeLength)
+	{
+		_destinationRange = destinationRange;
+		_sourceRange = sourceRange;
+        _sourceRangeEnd = sourceRange + rangeLength;
+	}
+
+	public int Apply(int input)
+    {
+        if (!(input >= _sourceRange && input < _sourceRangeEnd))
+        {
+            return input;
+        }
+
+        var diff = input - _sourceRange;
+        var output = _destinationRange + diff;
+
+        return output;
+    }
+}
---
+++ test/Aoc2023Day5Test/RuleTest.cs
@@ -0,0 +1,48 @@
+﻿using Aoc2023Day5;
+using FluentAssertions;
+
+namespace Aoc2023Day5Test;
+
+public class RuleTest
+{
+    /*
+      seeds: 79 14 55 13
+
+      seed-to-soil map:
+      50 98 2
+      52 50 48*/
+
+    [Theory]
+    [InlineData("50 98 2", 98, 50)]
+    [InlineData("50 98 2", 99, 51)]
+    [InlineData("50 98 2", 100, 100)]
+    [InlineData("50 98 2", 0, 0)]
+    [InlineData("50 98 2", 97, 97)]
+    [InlineData("50 98 2", 50, 50)]
+    
+    public void Given_Rule_When_Applied_Then_ReturnsExpectedValue(string line, int input, int expectedOutput)
+    {
+        var sut = new Rule(50, 98, 2);
+
+        var result = sut.Apply(input);
+
+        result.Should().Be(expectedOutput);
+
+    }
+
+    [Theory]
+    [InlineData("52 50 48", 0, 0)]
+    [InlineData("52 50 48", 1, 1)]
+    [InlineData("52 50 48", 49, 49)]
+    [InlineData("52 50 48", 50, 52)]
+    [InlineData("52 50 48", 51, 53)]
+    [InlineData("52 50 48", 97, 99)]
+    public void Given_Rule2_When_Applied_Then_ReturnsExpectedValue(string line, int input, int expectedOutput)
+    {
+        var sut = new Rule(52, 50, 48);
+
+        var result = sut.Apply(input);
+
+        result.Should().Be(expectedOutput);
+    }
+}
