b73d14e0b3435e1a0f1a1143ead461a829e0f79a
Frank Developr <frank.developr@gmail.com>
2023-12-07 18:27:50 +0100

    feature: part 1 working!

---
+++ src/Aoc2023Day7/TotalWinnigs.cs
@@ -0,0 +1,20 @@
+﻿namespace Aoc2023Day7;
+
+public class TotalWinnigs
+{
+    public long Winnings { get; }
+
+    public TotalWinnigs(IEnumerable<Hand> hands)
+    {
+        var sorted = hands.ToList();
+        sorted.Sort();
+
+        var result = 0L;
+        for (var i = 0; i < sorted.Count; i++)
+        {
+            result += sorted[i].Bid * (i+1);
+        }
+
+        Winnings = result;
+    }
+}
---
+++ test/Aoc2023Day7Test/TotalWinningsTest.cs
@@ -0,0 +1,20 @@
+﻿using Aoc2023Day7;
+using FluentAssertions;
+
+namespace Aoc2023Day7Test;
+
+public class TotalWinningsTest
+{
+    [Theory]
+    [InlineData("test7.txt", 6440)]
+    [InlineData("problem7.txt", 248569531L)]
+    public void Given_Hands_Then_CalculatesWinnings(string file, long expectedWinnings)
+    {
+        var lines = File.ReadAllLines($"data/{file}");
+        var reader = new HandReader(lines);
+
+        var sut = new TotalWinnigs(reader.Hands);
+        
+        sut.Winnings.Should().Be(expectedWinnings);
+    }
+}
