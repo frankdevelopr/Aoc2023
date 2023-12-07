a934ae6e1cad9b36d21de9c9d57638b3e8f87b78
Frank Developr <frank.developr@gmail.com>
2023-12-07 12:25:14 +0100

    feature: race error margin logic

--- src/Aoc2023Day6/RaceMarginError.cs
+++ src/Aoc2023Day6/RaceMarginError.cs
@@ -2,5 +2,18 @@
 
 public class RaceMarginError
 {
+    public int ErrorMargin { get; }
 
+    public RaceMarginError(IEnumerable<RaceSpec> raceSpecs)
+    {
+        var raceCalculator = new RaceCalculator();
+        var errorMargin = 1;
+
+        foreach (var raceSpec in raceSpecs)
+        {
+            errorMargin *= raceCalculator.WaysToWin(raceSpec);
+        }
+
+        ErrorMargin = errorMargin;
+    }
 }
--- test/Aoc2023Day6Test/RaceMarginErrorTest.cs
+++ test/Aoc2023Day6Test/RaceMarginErrorTest.cs
@@ -8,12 +8,8 @@ public class RaceMarginErrorTest
     [Fact]
     public void Given_Races_Then_ReturnsMarginOfError()
     {
-        /*var race = new RaceMarginError([new RaceSpec(7, 9), new RaceSpec(15, 40), new RaceSpec(30, 200)]);
+        var sut = new RaceMarginError([new RaceSpec(7, 9), new RaceSpec(15, 40), new RaceSpec(30, 200)]);
 
-        var wins = race.WaysToWin(7, 9);
-        wins *= race.WaysToWin(15, 40);
-        wins *= race.WaysToWin(30, 200);
-
-        wins.Should().Be(288);*/
+        sut.ErrorMargin.Should().Be(288);
     }
 }
