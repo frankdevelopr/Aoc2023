47cb5a0208fc6ad3d89d673fe8358d27ed65d748
Frank Developr <frank.developr@gmail.com>
2023-12-08 18:37:04 +0100

    feature: network

--- src/Aoc2023Day8/Network.cs
+++ src/Aoc2023Day8/Network.cs
@@ -1,5 +1,24 @@
-﻿namespace Aoc2023Day8;
+﻿
+namespace Aoc2023Day8;
 
 public class Network
 {
+    private readonly Dictionary<string, Node> _nodes = new();
+
+    public Network Add(Node node)
+    {
+        _nodes[node.Label] = node;
+
+        return this;
+    }
+
+    public Node? Get(string label)
+    {
+        if (_nodes.TryGetValue(label, out var node))
+        {
+            return node;
+        }
+
+        return null;
+    }
 }
---
+++ test/Aoc2023Day8Test/NetworkTest.cs
@@ -0,0 +1,25 @@
+﻿using Aoc2023Day8;
+using FluentAssertions;
+
+namespace Aoc2023Day8Test;
+
+public class NetworkTest
+{
+    [Fact]
+    public void Given_ValidNode_Returns_ExpectedNode()
+    {
+        var sut = new Network();
+
+        var nodeA = new Node("A", "B", "C");
+        var nodeB = new Node("B", "B", "C");
+        var nodeC = new Node("C", "A", "C");
+
+        sut.Add(nodeA).Add(nodeB).Add(nodeC);
+
+        sut.Get("A").Should().BeEquivalentTo(nodeA);
+        sut.Get("B").Should().BeEquivalentTo(nodeB);
+        sut.Get("C").Should().BeEquivalentTo(nodeC);
+
+        sut.Get("D").Should().BeNull();
+    }
+}
