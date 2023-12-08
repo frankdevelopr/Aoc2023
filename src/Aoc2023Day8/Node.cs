ca5d1770b482d28fce4cc1a633b54849691dccb9
Frank Developr <frank.developr@gmail.com>
2023-12-08 18:28:59 +0100

    feature: added node

---
+++ src/Aoc2023Day8/Node.cs
@@ -0,0 +1,15 @@
+﻿namespace Aoc2023Day8;
+
+public class Node
+{
+    public string Label { get; }
+    public string Left { get; }
+    public string Right { get; }
+
+    public Node(string label, string left, string right)
+    {
+        Label = label;
+        Left = left;
+        Right = right;
+    }
+}
