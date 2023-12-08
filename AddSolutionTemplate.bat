b3c66d1c510e74541279033d5f5523770a0e1031
Frank Developr <frank.developr@gmail.com>
2023-12-08 11:42:35 +0100

    chore: solution and improved template

--- AddSolutionTemplate.bat
+++ AddSolutionTemplate.bat
@@ -11,6 +11,13 @@ dotnet sln Day%1.sln add test/Aoc2023Day%1Test
 dotnet sln Aoc2023.sln add src/Aoc2023Day%1
 dotnet sln Aoc2023.sln add test/Aoc2023Day%1Test
 
+copy template\empty.txt src\Aoc2023Day%1\day%1.txt
+copy template\evaluation.txt src\Aoc2023Day%1\
+
+mkdir test\Aoc2023Day%1Test\data\ 
+copy template\empty.txt test\Aoc2023Day%1Test\data\problem.txt
+copy template\empty.txt test\Aoc2023Day%1Test\data\test.txt
+
 GOTO end
 
 :invalidParams
