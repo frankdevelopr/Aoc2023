ea85a7993180520575c7815877d85bf1a73538e9
Frank Developr <frank.developr@gmail.com>
2023-12-12 09:27:26 +0100

    feature: universe expander

--- src/Aoc2023Day11/Universe.cs
+++ src/Aoc2023Day11/Universe.cs
@@ -1,7 +1,12 @@
-﻿namespace Aoc2023Day11;
+﻿using System.Text;
+
+namespace Aoc2023Day11;
 
 public class Universe
 {
+    private static char EmptySpace = '.';
+    private static char Galaxy = '#';
+
     public string[] Expanded { get; }
 
     public Universe(string[] lines)
@@ -19,18 +24,79 @@ public class Universe
         return expanded;
     }
 
+    private string[] DoExpansion(string[] lines, IEnumerable<int> emptyRows, IEnumerable<int> emptyColumns)
+    {
+        var expanded = new string[lines.Length + emptyRows.Count()];
+
+        var targetRow = 0;
+
+        for (var y = 0; y < lines.Length; ++y)
+        {
+            var rowCreator = new StringBuilder();
+
+            for (var x = 0; x < lines[0].Length; ++x)
+            {
+                var c = lines[y][x];
+
+                rowCreator.Append(c);
+                if (emptyColumns.Contains(x))
+                {
+                    rowCreator.Append(EmptySpace);
+                }
+            }
+
+            expanded[targetRow] = rowCreator.ToString();
+            targetRow++;
+            
+            if (emptyRows.Contains(y))
+            {
+                expanded[targetRow++] = new string(EmptySpace, rowCreator.Length);
+            }
+        }
+
+        return expanded;
+    }
+
     private IEnumerable<int> FindEmptyRows(string[] lines)
     {
-        throw new NotImplementedException();
+        var empty = new List<int>();
+
+        for (var i = 0; i < lines.Length; i++)
+        {
+            if (lines[i].All(c => c == EmptySpace))
+            {
+                empty.Add(i);
+            }
+        }
+
+        return empty;
     }
 
     private IEnumerable<int> FindEmptyColumns(string[] lines)
     {
-        throw new NotImplementedException();
+        var empty = new List<int>();
+
+        for (var x = 0; x < lines[0].Length; ++x)
+        {
+            var isEmpty = true;
+
+            foreach (var line in lines)
+            {
+                isEmpty = line[x] == EmptySpace;
+                if (!isEmpty)
+                {
+                    break;
+                }
+            }
+
+            if (isEmpty)
+            {
+                empty.Add(x);
+            }
+        }
+
+        return empty;
     }
 
-    private string[] DoExpansion(string[] lines, IEnumerable<int> emptyRows, IEnumerable<int> emptyColumns)
-    {
-        throw new NotImplementedException();
-    }
+
 }
