2d1813e58e55d7ab8e6de2e1076594aa120a7b26
Frank Developr <frank.developr@gmail.com>
2023-12-15 16:50:52 +0100

    feature: combinator (taken from internet)

--- test/Aoc2023Day12Test/ArrangementsCounterUnfoldTest.cs
+++ test/Aoc2023Day12Test/ArrangementsCounterUnfoldTest.cs
@@ -16,4 +16,18 @@ public class ArrangementsCounterUnfoldTest
 
         sut.Arrangements.Should().Be(expectedArrangements);
     }
+
+    [Theory]
+    [InlineData("???.### 1,1,3", 1)]
+    [InlineData(".??..??...?##. 1,1,3", 16384)]
+    [InlineData("?#?#?#?#?#?#?#? 1,3,1,6", 1)]
+    [InlineData("????.#...#... 4,1,1", 16)]
+    [InlineData("????.######..#####. 1,6,5", 2500)]
+    [InlineData("?###???????? 3,2,1", 506250)]
+    public void Given_Line_When_Calculate_Then_ReturnsExpected(string line, long expectedArrangements)
+    {
+        var sut = new ArrangementsCounterUnfold([line]);
+
+        sut.Arrangements.Should().Be(expectedArrangements);
+    }
 }
