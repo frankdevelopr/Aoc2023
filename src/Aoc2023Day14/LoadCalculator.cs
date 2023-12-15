7ac0cd3b7decdebd1ab759467bc1034774dd24d4
Frank Developr <frank.developr@gmail.com>
2023-12-15 17:41:32 +0100

    feat: load calculator

---
+++ src/Aoc2023Day14/LoadCalculator.cs
@@ -0,0 +1,27 @@
+﻿namespace Aoc2023Day14;
+
+public class LoadCalculator
+{
+    private readonly string[] _lines;
+
+    public LoadCalculator(string[] lines)
+    {
+        _lines = lines;
+    }
+
+    public long Calculate()
+    {
+        var sum = 0L;
+        var length = _lines.Length;
+
+        for (var i = 0; i < length; ++i)
+        {
+            var multiplier = length - i;
+
+            var roundedRocks = _lines[i].Count(t => t == 'O') * multiplier;
+            sum += roundedRocks;
+        }
+
+        return sum;
+    }
+}
