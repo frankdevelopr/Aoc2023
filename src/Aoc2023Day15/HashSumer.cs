0566c6304f9e9142b4566cc3b752af1d0a0485ee
Frank Developr <frank.developr@gmail.com>
2023-12-17 11:44:09 +0100

    feature: hashsumer working, part 1 done

--- src/Aoc2023Day15/HashSumer.cs
+++ src/Aoc2023Day15/HashSumer.cs
@@ -1,9 +1,24 @@
-﻿namespace Aoc2023Day15;
+﻿using System.Diagnostics;
+
+namespace Aoc2023Day15;
 
 public class HashSumer
 {
     public long Sum(string line)
     {
-        throw new NotImplementedException();
+        var parts = line.Split(',').Select(s => s.Trim());
+
+        var hash = new Hash();
+        var total = 0L;
+
+        Parallel.ForEach(parts, part =>
+        {
+            var value = hash.Calculate(part);
+            Debug.WriteLine($"{part} = {value}");
+
+            Interlocked.Add(ref total, value);
+        });
+
+        return total;
     }
 }
--- test/Aoc2023Day15Test/HashSumerTest.cs
+++ test/Aoc2023Day15Test/HashSumerTest.cs
@@ -7,6 +7,8 @@ public class HashSumerTest
 {
     [Theory]
     [InlineData("data/test.txt", 1320)]
+    [InlineData("data/problem.txt", 514025L)]
+
     public void Given_Input_Then_ReturnsExpectedHashSum(string file, long expected)
     {
         var line = File.ReadAllText(file);
