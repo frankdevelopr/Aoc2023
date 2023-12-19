147dcebc225c1c3e2de42d1d29f7ca1288ebe090
Frank Developr <frank.developr@gmail.com>
2023-12-19 20:04:20 +0100

    feat: TrenchSize calculator

---
+++ src/Aoc2023Day18/TrenchSize.cs
@@ -0,0 +1,84 @@
+﻿namespace Aoc2023Day18;
+
+public class TrenchSize
+{
+    public int Height { get; private set; }
+    public int Width { get; private set; }
+    public Position InitialPosition { get; private set; }
+
+    private readonly string[] _lines;
+
+    public TrenchSize(string[] lines)
+    {
+        _lines = lines;
+        Evaluate();
+    }
+
+    private void Evaluate()
+    {
+        var current = new Position(0, 0);
+        var positions = new List<Position>([current]);
+
+        foreach (var line in _lines)
+        {
+            var instruction = new Instruction(line);
+
+            switch (instruction.Direction)
+            {
+                case 'U':
+                    current = new Position(current.Y-instruction.Positions, current.X);
+                    break;
+                case 'D':
+                    current = new Position(current.Y+instruction.Positions, current.X);
+                    break;
+                case 'L':
+                    current = new Position(current.Y, current.X-instruction.Positions);
+                    break;
+                case 'R':
+                    current = new Position(current.Y, current.X+instruction.Positions);
+                    break;
+            }
+
+            positions.Add(current);
+        }
+
+        var upMost = positions.Select(t => t.Y).Min();
+        var downMost = positions.Select(t => t.Y).Max();
+
+        var leftMost = positions.Select(t => t.X).Min();
+        var rightMost = positions.Select(t => t.X).Max();
+
+        Height = downMost - upMost + 1;
+        Width = rightMost - leftMost + 1;
+
+        InitialPosition = new Position(-upMost, -leftMost);
+    }
+}
+
+public class Position
+{
+    public int Y { get; }
+    public int X { get; }
+
+    public Position(int y, int x)
+    {
+        Y = y; 
+        X = x;
+    }
+}
+
+public class Instruction
+{
+    public char Direction { get; }
+    public int Positions { get; }
+    public string Color { get; }
+
+    public Instruction(string line)
+    {
+        var parts = line.Split(' ').Select(t => t.Trim()).ToArray();
+
+        Direction = parts[0][0];
+        Positions = int.Parse(parts[1]);
+        Color = parts[2];
+    }
+}
---
+++ test/Aoc2023Day18Test/TrenchSizeTest.cs
@@ -0,0 +1,22 @@
+using Aoc2023Day18;
+using FluentAssertions;
+
+namespace Aoc2023Day18Test;
+
+public class TrenchSizeTest
+{
+    [Theory]
+    [InlineData("data/test.txt", 10, 7, 0, 0)]
+    [InlineData("data/test-2.txt", 6, 7, 5, 6)]
+    [InlineData("data/test-3.txt", 6, 11, 5, 8)]
+    public void Given_ValidTrenchInstructions_Then_SizesAreWellCalculated(string file, int height, int width, int y, int x)
+    {
+        var lines = File.ReadAllLines(file);
+
+        var sut = new TrenchSize(lines);
+        sut.Height.Should().Be(height);
+        sut.Width.Should().Be(width);
+
+        sut.InitialPosition.Should().BeEquivalentTo(new Position(y, x));
+    }
+}
