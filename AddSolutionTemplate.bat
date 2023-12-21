9436b180ed68b1c6ec6406bd6aa8a0ec6c7797f2
Frank Developr <frank.developr@gmail.com>
2023-12-21 08:16:42 +0100

    chore: project utils added to new solutions

--- AddSolutionTemplate.bat
+++ AddSolutionTemplate.bat
@@ -5,6 +5,7 @@ dotnet new sln --name Day%1
 dotnet new console -n Aoc2023Day%1 -o src/Aoc2023Day%1
 dotnet new xunit -n Aoc2023Day%1Test -o test/Aoc2023Day%1Test
 
+dotnet sln Day%1.sln add src/Utils
 dotnet sln Day%1.sln add src/Aoc2023Day%1
 dotnet sln Day%1.sln add test/Aoc2023Day%1Test
 
